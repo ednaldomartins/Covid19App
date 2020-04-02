af00315a7a246ba18250fd6c953ce6aca54e480c
Ednaldo <martinsednaldo@outlook.com>
2020-04-02 18:25:17 -0300

    Navigation component.

---
+++ app/src/main/java/com/ednaldomartins/covid19app/presentation/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ednaldomartins.covid19app.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ednaldomartins.covid19app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
