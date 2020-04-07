eee85001b06799010dee83e536b6b482de1354c5
Ednaldo <martinsednaldo@outlook.com>
2020-04-07 11:28:25 -0300

    Adiantando processo de requisição da lista antes de chamar o fragment que apresenta a lista.

--- app/src/main/java/com/ednaldomartins/covid19app/presentation/fragment/SplashScreenFragment.kt
+++ app/src/main/java/com/ednaldomartins/covid19app/presentation/fragment/SplashScreenFragment.kt
@@ -6,7 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 
@@ -25,19 +24,15 @@ class SplashScreenFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_splash_screen, container, false)
 
+        val application = requireNotNull(this.activity).application
+        countryViewModelFactory = ViewModelFactory(application)
+        countryListApiViewModel = ViewModelProvider(activity!!, countryViewModelFactory).get(CountryListApiViewModel::class.java)
+
+        //  chamar a lista da API enquanto apresenta telas de boas vindas
+        countryListApiViewModel.requestCountryList()
         Handler().postDelayed({
-            val application = requireNotNull(this.activity).application
-            countryViewModelFactory = ViewModelFactory(application)
-            countryListApiViewModel = ViewModelProvider(activity!!, countryViewModelFactory).get(CountryListApiViewModel::class.java)
-
-            //  chamar a lista da API enquanto apresenta telas de boas vindas
-            countryListApiViewModel.requestCountryList()
-
-            //  observador aguarda o necessario para chamar a transicao
-            countryListApiViewModel.responseCountryList.observe(this.viewLifecycleOwner, Observer {
-                view.findNavController().navigate(R.id.action_splashScreenFragment_to_countryListFragment)
-            })
-        },500)  //  tempo minimo que eu darei ao splash
+            view.findNavController().navigate(R.id.action_splashScreenFragment_to_countryListFragment)
+        },1000)  //  tempo minimo que eu darei ao splash
 
         return view
     }
