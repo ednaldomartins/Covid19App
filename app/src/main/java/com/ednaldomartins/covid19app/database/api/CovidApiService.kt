f8c4d3ba07f0f798c0a1db993c5567815d3db1ed
Ednaldo <martinsednaldo@outlook.com>
2020-04-07 11:26:45 -0300

    Novo @Get para pegar informações dos casos de cada país por situação (Status).

--- app/src/main/java/com/ednaldomartins/covid19app/database/api/CovidApiService.kt
+++ app/src/main/java/com/ednaldomartins/covid19app/database/api/CovidApiService.kt
@@ -1,6 +1,7 @@
 package com.ednaldomartins.covid19app.database.api
 
-import com.ednaldomartins.covid19app.domain.entity.CountryJson
+import com.ednaldomartins.covid19app.domain.entity.CountryListJson
+import com.ednaldomartins.covid19app.domain.entity.CountryStatusJson
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -8,8 +9,9 @@ import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
-private val URL_API: String = "https://api.covid19api.com/"
+private const val URL_API: String = "https://api.covid19api.com/"
 
 private val  moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -22,9 +24,16 @@ private val  retrofit = Retrofit.Builder()
     .build()
 
 interface CovidApiService {
+
     // chamar lista de paises
     @GET("summary")
-    fun callCountryList (): Deferred<CountryJson>
+    fun callCountryListAsync (): Deferred<CountryListJson>
+
+    @GET("total/country/{country}/status/{status}")
+    fun callCountryStatusAsync (
+        @Path("country") country : String,
+        @Path("status") status : String
+    ): Deferred<List<CountryStatusJson>>
 
 }
 
